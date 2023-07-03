9960433d2c8b0627c3edb7359e28b01b4e944b61
Ian Baldwin <mindofianb@gmail.com>
2023-07-03 13:51:43 -0600

    FastAPI for Pokemon Complete

---
+++ fast-api/pokemon-api/main.py
@@ -0,0 +1,24 @@
+import json
+import httpx
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+poke_url = "https://pokeapi.co/api/v2/pokemon/"
+
+
+class Pokemon(BaseModel):
+    name: str
+    id: int
+
+
+async def get_pokemon_from_api(poke_id: int):
+    async with httpx.AsyncClient(verify=False) as client:
+        response = await client.get(poke_url + str(poke_id))
+        return response
+
+@app.get("/{poke_id}")
+async def get_pokemon_by_id(poke_id: int) -> Pokemon:
+    response = await get_pokemon_from_api(poke_id)
+    pokemon = response.json()
+    return Pokemon(id=poke_id, name=pokemon['forms'][0]['name'])
