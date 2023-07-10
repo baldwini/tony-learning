63e48339e46327fbae3c7bccc19a185e8124261f
Ian Baldwin <mindofianb@gmail.com>
2023-07-10 14:44:55 -0600

    Redis implemented and updated calls

---
+++ fast-api/pokemon-api/app/api/pokemon_api.py
@@ -0,0 +1,58 @@
+import httpx
+import uvicorn
+import json
+import redis
+from fastapi import FastAPI, Request, Response
+
+from app.routers import pokemon_router
+
+
+class PokemonApi:
+    def __init__(self):
+
+        app = FastAPI(
+            title="Pokemon API",
+            description="Allows you to access Pokemon by ID.",
+            version="1.0.0"
+        )
+        app.include_router(pokemon_router.router)
+        self.uv_server = uvicorn.Server(
+            uvicorn.Config(
+                app=app,
+                host="0.0.0.0",
+            )
+        )
+        self.client = httpx.AsyncClient(verify=False)
+        self.redis_db = redis.Redis(
+            host='127.0.0.1',
+            port=6379,
+            db=0
+        )
+
+        @app.on_event("startup")
+        async def startup_event():
+            print("Starting Up...")
+
+        @app.middleware("http")
+        async def pokemon_middleware(request: Request, call_next):
+            request.state.client = self.client
+            request.state.redis_db = self.redis_db
+            response = await call_next(request)
+            return response
+
+            #Load response_body courtesy of https://stackoverflow.com/a/71883126
+            #response_body = b""
+            #async for chunk in response.body_iterator:
+            #    response_body += chunk
+            #print(f"response_body={response_body.decode()}")
+
+            #Set response in Redis
+            #self.r.set(request.path_params['poke_id'], response_body.decode())
+
+            #return Response(content=response_body, status_code=response.status_code,
+            #                headers=dict(response.headers), media_type=response.media_type)
+
+
+if __name__ == "__main__":
+    pokemon_api = PokemonApi()
+    pokemon_api.uv_server.run()
---
+++ fast-api/pokemon-api/app/routers/pokemon_router.py
@@ -0,0 +1,55 @@
+"""
+    Pokemon Router API Endpoint
+"""
+# from fastapi_utils.inferring_router import InferringRouter
+# from fastapi_utils.cbv import cbv
+
+from fastapi import APIRouter, Request, HTTPException
+import json
+
+from app.models.pokemon_api_models import Pokemon
+
+poke_url = "https://pokeapi.co/api/v2/pokemon/"
+
+router = APIRouter()
+
+
+class PokemonRouter:
+    @router.get(
+        path="/{poke_id}",
+        response_model=Pokemon,
+        description="Gets Pokemon from Redis",
+    )
+    async def get_pokemon_by_id(request: Request, poke_id: int) -> Pokemon:
+        response = request.state.redis_db.get(poke_id)
+        if response is None:
+            HTTPException(status_code=404, detail="Item not found")
+        pokemon = json.loads(response)
+        return Pokemon(pokemon)
+
+    @router.post(
+        path="/{poke_id}",
+        response_model=Pokemon,
+        description="Creates new Pokemon in Redis by retrieving it from Pokeapi"
+    )
+    async def create_pokemon(request: Request, poke_id: int) -> Pokemon:
+        if request.state.redis_db.get(poke_id) is not None:
+            raise HTTPException(status_code=409, detail="Item already exists")
+        response = await request.state.client.get(poke_url + str(poke_id))
+        pokemon = response.json()
+        pokemon_obj = Pokemon(id=poke_id, name=pokemon['forms'][0]['name'])
+        request.state.redis_db.set(request.path_params['poke_id'], pokemon_obj.json())
+        return pokemon_obj
+
+    @router.delete(
+        path="/{poke_id}",
+        response_model=Pokemon,
+        description="Deletes Pokemon in Redis"
+    )
+    async def delete_pokemon(request: Request, poke_id: int) -> Pokemon:
+        response = request.state.redis_db.get(poke_id)
+        if response is None:
+            HTTPException(status_code=404, detail="Item not found")
+        request.state.redis_db.delete(poke_id)
+        pokemon = json.loads(response)
+        return Pokemon(pokemon)
