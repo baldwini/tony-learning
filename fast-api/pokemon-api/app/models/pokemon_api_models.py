8077fae6de4e39adbe76c094bdc52a699bb5cba2
Ian Baldwin <mindofianb@gmail.com>
2023-07-27 17:03:20 -0600

    Built out models for storing trace_id in requests and responses

--- fast-api/pokemon-api/app/models/pokemon_api_models.py
+++ fast-api/pokemon-api/app/models/pokemon_api_models.py
@@ -4,3 +4,13 @@ from pydantic import BaseModel
 class Pokemon(BaseModel):
     name: str
     id: int
+
+
+class Transaction(BaseModel):
+    trace_id: str
+    pokemon: Pokemon
+
+class ApiResponse(BaseModel):
+    trace_id: str
+    status_code: str
+    message: str
