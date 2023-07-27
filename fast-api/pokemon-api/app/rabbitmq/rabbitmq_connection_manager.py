0f0fb53c7541f9c36905f52a8d7cbb4979fe8d7b
Ian Baldwin <mindofianb@gmail.com>
2023-07-27 17:02:34 -0600

    Modified rabbitmq startup functions

--- fast-api/pokemon-api/app/rabbitmq/rabbitmq_connection_manager.py
+++ fast-api/pokemon-api/app/rabbitmq/rabbitmq_connection_manager.py
@@ -7,7 +7,8 @@ from typing import Optional
 class RabbitMQConnectionManager:
 
     def __init__(self):
-        self.exchange: str = 'pokemon_exchange'
+        self.pokemon_exchange: str = 'pokemon_exchange'
+        self.kafka_exchange: str = 'kafka_exchange'
         self.connection: Optional[Connection] = None
         self.channel: Optional[AbstractChannel] = None
 
@@ -15,20 +16,38 @@ class RabbitMQConnectionManager:
         self.connection = await aiormq.connect(url="amqp://guest:guest@rmq/")
         self.channel = await self.connection.channel()
 
+    async def create_pokemon_exchange(self):
         await self.channel.exchange_declare(
-            exchange=self.exchange,
+            exchange=self.pokemon_exchange,
             exchange_type='direct'
         )
 
-    async def define_queue(self, command: str):
+    async def create_kafka_exchange(self):
+        await self.channel.exchange_declare(
+            exchange=self.kafka_exchange,
+            exchange_type='x-consistent-hash'
+        )
+
+    async def define_pokemon_queue(self, command: str):
         queue = await self.channel.queue_declare(
             queue=command+'_queue',
         )
 
         await self.channel.queue_bind(
             queue=queue.queue,
-            exchange=self.exchange,
+            exchange=self.pokemon_exchange,
             routing_key=command
+        )
+
+    async def define_kafka_queue(self, weight: int):
+        queue = await self.channel.queue_declare(
+            queue='kafka_queue'
+        )
+
+        await self.channel.queue_bind(
+            queue=queue.queue,
+            exchange=self.kafka_exchange,
+            routing_key=str(weight)
         )
 
     async def define_callback_queue(self, command: str):
@@ -37,6 +56,6 @@ class RabbitMQConnectionManager:
         )
         await self.channel.queue_bind(
             queue=callback_queue.queue,
-            exchange=self.exchange,
+            exchange=self.pokemon_exchange,
             routing_key=command+'_callback'
         )
