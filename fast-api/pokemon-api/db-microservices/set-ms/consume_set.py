32486b4b774d777fa8d8d2e90701d7fcbab9cad7
Ian Baldwin <mindofianb@gmail.com>
2023-07-28 10:13:08 -0600

    Update set consumer

--- fast-api/pokemon-api/db-microservices/set-ms/consume_set.py
+++ fast-api/pokemon-api/db-microservices/set-ms/consume_set.py
@@ -7,7 +7,7 @@ from app.rabbitmq.rabbitmq_connection_manager import RabbitMQConnectionManager
 
 rmq = RabbitMQConnectionManager()
 redis_db: Redis = Redis(
-    host='redis_db',
+    host='127.0.0.1',
     port=6379,
     db=0
 )
@@ -16,15 +16,23 @@ redis_db: Redis = Redis(
 async def callback(message: DeliveredMessage):
     transaction: Transaction = Transaction.parse_raw(message.body)
     pokemon: Pokemon = transaction.pokemon
+    status_code: str = "200"
+    error_message: str
+    response: ApiResponse
 
-    redis_db.set(pokemon.id, pokemon.json())
+    if redis_db.get(pokemon.id):
+        status_code = "409"
+        error_message = "Database resource already exists"
+        response = ApiResponse(trace_id=transaction.trace_id, status_code=status_code, message=error_message)
 
-    response: ApiResponse = ApiResponse(trace_id=transaction.trace_id, status_code="200", message=pokemon)
+    else:
+        redis_db.set(pokemon.id, pokemon.json())
+        response = ApiResponse(trace_id=transaction.trace_id, status_code=status_code, message=pokemon.json())
 
     await rmq.channel.basic_publish(
         exchange="kafka_exchange",
-        routing_key="kafka_key",
-        body=response.json().encode('ACSII')
+        routing_key=str(pokemon.id),
+        body=response.json().encode('ASCII')
     )
 
 
@@ -32,6 +40,7 @@ async def main():
     await rmq.create()
     await rmq.create_pokemon_exchange()
     await rmq.define_pokemon_queue(command='set')
+    await rmq.create_kafka_exchange()
     await rmq.define_kafka_queue(weight=1)
     await rmq.channel.basic_qos(prefetch_count=1)
     await rmq.channel.basic_consume(queue='set_queue', consumer_callback=callback, no_ack=True)
