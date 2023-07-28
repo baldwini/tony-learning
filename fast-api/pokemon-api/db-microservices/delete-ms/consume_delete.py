f38b37713cbd079fde49b3ef67838bae27679b6d
Ian Baldwin <mindofianb@gmail.com>
2023-07-28 10:16:06 -0600

    Update delete consumer

--- fast-api/pokemon-api/db-microservices/delete-ms/consume_delete.py
+++ fast-api/pokemon-api/db-microservices/delete-ms/consume_delete.py
@@ -2,27 +2,43 @@ from redis import Redis
 from aiormq.abc import DeliveredMessage
 import asyncio
 
-from app.models.pokemon_api_models import Pokemon
+from app.models.pokemon_api_models import Pokemon, ApiResponse, Transaction
 from app.rabbitmq.rabbitmq_connection_manager import RabbitMQConnectionManager
 
 
 rmq = RabbitMQConnectionManager()
 redis_db: Redis = Redis(
-    host='redis_db',
+    host='localhost',
     port=6379,
     db=0
 )
 
 
 async def callback(message: DeliveredMessage):
-    poke_id = message.body.decode('ASCII')
-    redis_db.delete(poke_id)
+    transaction: Transaction = Transaction.parse_raw(message.body)
+    pokemon: Pokemon = transaction.pokemon
+    status_code: str = "200"
+    error_message: str
+    response: ApiResponse
+
+    if not redis_db.get(pokemon.id):
+        status_code = "404"
+        error_message = "Pokemon not found"
+        response = ApiResponse(trace_id=transaction.trace_id, status_code=status_code, message=error_message)
+
+    else:
+        redis_db.delete(pokemon.id)
+        response = ApiResponse(trace_id=transaction.trace_id, status_code=status_code, message=pokemon.json())
+
+    return response
 
 
 async def main():
     await rmq.create()
     await rmq.create_pokemon_exchange()
     await rmq.define_pokemon_queue(command='delete')
+    await rmq.create_kafka_exchange()
+    await rmq.define_kafka_queue(weight=1)
     await rmq.channel.basic_qos(prefetch_count=1)
     await rmq.channel.basic_consume(queue='delete_queue', consumer_callback=callback, no_ack=True)
 
